4f29a7639bfa8f15eeb03016e34d01a33c965958
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-05-31 17:09:58 +0100

    removed logic from controller

---
+++ controllers.js
@@ -0,0 +1,16 @@
+toDoApp.controller("ToDoController", ['ToDoFactory', function (ToDoFactory){
+
+  var self = this;
+
+  self.todos = [];
+
+  self.addToDo = function(todoText){
+    self.todos.push(new ToDoFactory(todoText));
+
+  };
+
+  self.removeToDo = function(){
+    self.todos.pop();
+  }
+
+}]);
